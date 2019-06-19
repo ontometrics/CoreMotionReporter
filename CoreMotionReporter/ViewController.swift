ef1d56317e47bc59b616144c87a4b67b0a73cb94
rob <rob@ontometrics.com>
2019-06-19 11:20:26 -0700

    Initial Commit

---
+++ CoreMotionReporter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMotionReporter
+//
+//  Created by Rob Williams on 6/19/19.
+//  Copyright © 2019 Rob Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
